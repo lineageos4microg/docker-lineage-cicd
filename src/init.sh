7142edf4dd41d972762a84f02edbf9d9284275e0
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-01-12 23:51:01 +0100

    printenv output was not compatible with crontab
    Triage by sedding the output in a proper way

--- src/init.sh
+++ src/init.sh
@@ -16,8 +16,8 @@ git config --global user.email $USER_MAIL
 # Initialize the cronjob
 cronFile=/tmp/buildcron
 printf "SHELL=/bin/bash\n" > $cronFile
-printenv >> $cronFile
-printf "\n\n$CRONTAB_TIME /usr/bin/flock -n /tmp/lock.build /root/build.sh 2>&1\n" >> $cronFile
+printenv -0 | sed -e 's/=\x0/=""\n/g'  | sed -e 's/\x0/\n/g' | sed -e "s/_=/PRINTENV=/g" >> $cronFile
+printf "\n$CRONTAB_TIME /usr/bin/flock -n /tmp/lock.build /root/build.sh 2>&1\n" >> $cronFile
 crontab $cronFile
 rm $cronFile
 
