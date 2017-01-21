1bf0736112b2aed9bb86470d2fafa970a5f174c5
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-01-22 00:28:03 +0100

    Wrong property set for the OTA URL
    We should use PRODUCT_PROPERTY_OVERRIDES which seems to be kept until the final stage where the build.prop is being built

--- src/build.sh
+++ src/build.sh
@@ -49,7 +49,7 @@ if ! [ -z "$DEVICE_LIST" ]; then
   # Set a custom updater URI if a OTA URL is provided
   if ! [ -z "$OTA_URL" ]; then
     echo ">> [$(date)] Adding OTA URL '$OTA_URL' to build.prop"
-    sed -i "1s;^;ADDITIONAL_DEFAULT_PROPERTIES += cm.updater.uri=$OTA_URL\n\n;" vendor/cm/config/common.mk
+    sed -i "1s;^;PRODUCT_PROPERTY_OVERRIDES += cm.updater.uri=$OTA_URL\n\n;" vendor/cm/config/common.mk
   fi
 
   # Cycle DEVICE_LIST environment variable, to know which one may be executed next
