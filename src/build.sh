d0d4576ed9f3b48ede7bea2e0e774b1aac73ec16
Nicola Corna <nicola@corna.info>
2017-06-06 08:27:29 +0200

    Change license to GPLv3

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
+# Docker build script
+# Copyright (c) 2017 Julian Xhokaxhiu
+# Copyright (C) 2017 Nicola Corna <nicola@corna.info>
 #
-# Build script
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-###########################################################
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 DOCKER_LOG=/var/log/docker.log
 DEBUG_LOG=/dev/null
