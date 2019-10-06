64b6d197060e42128ff244720eb1f1601fded60b
leonghui <leonghui@users.noreply.github.com>
2019-10-06 04:40:38 +0000

    Add keys for networkstack
    Resolves error
    FAILED: ninja: 'user-keys/networkstack.x509.pem', needed by 'out/soong/.intermediates/packages/modules/CaptivePortalLogin/CaptivePortalLogin/android_common/CaptivePortalLogin.apk', missing and no known rule to make it
    
    See:
    https://android-review.googlesource.com/q/I7257a472d702e82acdadffc4e0535c63a8bd591d

--- src/init.sh
+++ src/init.sh
@@ -35,12 +35,12 @@ git config --global user.email $USER_MAIL
 if [ "$SIGN_BUILDS" = true ]; then
   if [ -z "$(ls -A "$KEYS_DIR")" ]; then
     echo ">> [$(date)] SIGN_BUILDS = true but empty \$KEYS_DIR, generating new keys"
-    for c in releasekey platform shared media; do
+    for c in releasekey platform shared media networkstack; do
       echo ">> [$(date)]  Generating $c..."
       /root/make_key "$KEYS_DIR/$c" "$KEYS_SUBJECT" <<< '' &> /dev/null
     done
   else
-    for c in releasekey platform shared media; do
+    for c in releasekey platform shared media networkstack; do
       for e in pk8 x509.pem; do
         if [ ! -f "$KEYS_DIR/$c.$e" ]; then
           echo ">> [$(date)] SIGN_BUILDS = true and not empty \$KEYS_DIR, but \"\$KEYS_DIR/$c.$e\" is missing"
