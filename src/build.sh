a45a81ee2e3bfe22aa8d8ff5430aa21b2da73363
Philip Nagler-Frank <philip@nagler.world>
2022-02-08 21:13:01 +0100

    don't fail when no old logs to delete

--- src/build.sh
+++ src/build.sh
@@ -397,7 +397,7 @@ for branch in ${BRANCH_NAME//,/ }; do
 done
 
 if [ "$DELETE_OLD_LOGS" -gt "0" ]; then
-  find "$LOGS_DIR" -maxdepth 1 -name 'repo-*.log' | sort | head -n -"$DELETE_OLD_LOGS" | xargs -r rm
+  find "$LOGS_DIR" -maxdepth 1 -name 'repo-*.log' | sort | head -n -"$DELETE_OLD_LOGS" | xargs -r rm || true
 fi
 
 if [ -f /root/userscripts/end.sh ]; then
