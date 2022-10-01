6f385b1f6b5c777f0646c7a6259ce1b15c4c29c4
Philip Nagler-Frank <philip@nagler.world>
2022-10-01 21:55:15 +0200

    fix lineage-20.0 branch name

--- src/build.sh
+++ src/build.sh
@@ -136,8 +136,8 @@ for branch in ${BRANCH_NAME//,/ }; do
         frameworks_base_patch="android_frameworks_base-S.patch"
         modules_permission_patch="packages_modules_Permission-S.patch"
         ;;
-      lineage-20*)
-        themuppets_branch="lineage-20"
+      lineage-20.0*)
+        themuppets_branch="lineage-20.0"
         android_version="13"
         frameworks_base_patch="android_frameworks_base-Android13.patch"
         modules_permission_patch="packages_modules_Permission-Android13.patch"
