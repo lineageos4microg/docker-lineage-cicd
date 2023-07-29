0c98820f66bc8e6ee911bde91f4015dcc143ebef
Pete Fotheringham <petefoth@e.email>
2023-07-29 18:27:55 +0100

    Update build.sh
    Correct my shell errors

--- src/build.sh
+++ src/build.sh
@@ -346,7 +346,7 @@ for branch in ${BRANCH_NAME//,/ }; do
             # call post-build.sh so the failure is logged in a way that is more visible
             if [ -f /root/userscripts/post-build.sh ]; then
               echo ">> [$(date)] Running post-build.sh for $codename" >> "$DEBUG_LOG"
-              /root/userscripts/post-build.sh "$codename $branch $build_successful" &>> "$DEBUG_LOG" || echo ">> [$(date)] Warning: post-build.sh failed!"
+              /root/userscripts/post-build.sh "$codename false $branch" &>> "$DEBUG_LOG" || echo ">> [$(date)] Warning: post-build.sh failed!"
             fi
             continue
         fi
@@ -404,7 +404,7 @@ for branch in ${BRANCH_NAME//,/ }; do
         fi
         if [ -f /root/userscripts/post-build.sh ]; then
           echo ">> [$(date)] Running post-build.sh for $codename" >> "$DEBUG_LOG"
-          /root/userscripts/post-build.sh "$codename $branch" $build_successful &>> "$DEBUG_LOG" || echo ">> [$(date)] Warning: post-build.sh failed!"
+          /root/userscripts/post-build.sh "$codename  $build_successful $branch" &>> "$DEBUG_LOG" || echo ">> [$(date)] Warning: post-build.sh failed!"
         fi
         echo ">> [$(date)] Finishing build for $codename" | tee -a "$DEBUG_LOG"
 
