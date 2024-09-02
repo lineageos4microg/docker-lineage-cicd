685f897d307646c7e3e67c641b7424d930636eee
Pete Fotheringham <petefoth@e.email>
2024-09-02 08:10:57 +0100

    Add new_build.sh

---
+++ src/new_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Slimmed-down Docker build script
+# Copyright (c) 2017 Julian Xhokaxhiu
+# Copyright (C) 2017-2018 Nicola Corna <nicola@corna.info>
+# Copyright (C) 2024 Pete Fotheringham <petefoth@e.email>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
