657c2a1ba3e8199ceda2d0c72aac442ce3f64f46
Pete Fotheringham <petefoth@e.email>
2025-12-18 09:32:54 +0000

    generate 4096 bit apex signing keys if needed

--- src/init.sh
+++ src/init.sh
@@ -35,10 +35,19 @@ git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_MAIL"
 
 if [ "$SIGN_BUILDS" = true ]; then
+  # generate the main signing keys
   for c in bluetooth media networkstack nfc platform releasekey sdk_sandbox shared testcert verity ; do
     if [ ! -f "$KEYS_DIR/$c.pk8" ]; then
       echo ">> [$(date)]  Generating $c..."
       /root/make_key "$KEYS_DIR/$c" "$KEYS_SUBJECT" <<< '' &> /dev/null
+    fi
+  done
+
+  # generate the apex signing keys
+  for c in com.android.adbd com.android.adservices com.android.adservices.api com.android.appsearch com.android.appsearch.apk com.android.art com.android.bluetooth com.android.bt com.android.btservices com.android.cellbroadcast com.android.compos com.android.configinfrastructure com.android.connectivity.resources com.android.conscrypt com.android.crashrecovery com.android.devicelock com.android.extservices com.android.graphics.pdf com.android.hardware.authsecret com.android.hardware.biometrics.face.virtual com.android.hardware.biometrics.fingerprint.virtual com.android.hardware.boot com.android.hardware.cas com.android.hardware.contexthub com.android.hardware.dumpstate com.android.hardware.gatekeeper.nonsecure com.android.hardware.neuralnetworks com.android.hardware.power com.android.hardware.rebootescrow com.android.hardware.thermal com.android.hardware.threadnetwork com.android.hardware.uwb com.android.hardware.vibrator com.android.hardware.wifi com.android.healthfitness com.android.hotspot2.osulogin com.android.i18n com.android.ipsec com.android.media com.android.media.swcodec com.android.mediaprovider com.android.nearby.halfsheet com.android.networkstack.tethering com.android.neuralnetworks com.android.nfcservices com.android.ondevicepersonalization com.android.os.statsd com.android.permission com.android.profiling com.android.resolv com.android.rkpd com.android.runtime com.android.safetycenter.resources com.android.scheduling com.android.sdkext com.android.support.apexer com.android.telephony com.android.telephonycore com.android.telephonymodules com.android.tethering com.android.tzdata com.android.uprobestats com.android.uwb com.android.uwb.resources com.android.virt com.android.vndk.current com.android.vndk.current.on_vendor com.android.wifi com.android.wifi.dialog com.android.wifi.resources com.google.pixel.camera.hal com.google.pixel.vibrator.hal com.qorvo.uwb ; do
+    if [ ! -f "$KEYS_DIR/$c.pk8" ]; then
+      echo ">> [$(date)]  Generating $c..."
+      /root/make_key "$KEYS_DIR/$c" "$KEYS_SUBJECT"  apex <<< '' &> /dev/null
     fi
   done
 
